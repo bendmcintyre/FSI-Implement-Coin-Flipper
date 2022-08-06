f5766d6422b1ef178165f11479399fb59cad7653
Ben McIntyre <ben@bens-mbp.attlocal.net>
2022-08-05 18:59:49 -0700

    Dice Rolling Game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,43 @@
 //Variables declared
-let heads = 0;
-let tails = 0;
-let headPercent = 0;
-let tailPercent = 0;
-let total = heads + tails;
+let rolls = [0, 0, 0, 0, 0, 0];
+
+//Scoreboard percentage calculator
+let scoreboard = function () {
+  let total = rolls[0] + rolls[1] + rolls[2] + rolls[3] + rolls[4] + rolls[5];
+  let percent = [0, 0, 0, 0, 0, 0];
+
+  if (total > 0) {
+    for (let i = 0; i < rolls.length; i++)
+      percent[i] = Math.round((rolls[i] / total) * 100);
+  }
+
+  for (let i = 0; i < rolls.length; i++) {
+    document.querySelector(`#td${i + 1}`).textContent = rolls[i];
+    document.querySelector(`#td${i + 1}-percent`).textContent =
+      percent[i] + "%";
+  }
+};
 
 document.addEventListener("DOMContentLoaded", function () {
   //Play game
-  document.querySelector("#flip").addEventListener("click", function () {
-    let randomChance = Math.random();
+  document.querySelector("#roll").addEventListener("click", function () {
+    let rollNumber = Math.ceil(Math.random() * 6);
+    document.querySelector(
+      "#message"
+    ).textContent = `You Rolled ${rollNumber}!`;
+    rolls[rollNumber - 1] += 1;
+    document.querySelector(
+      "#dice-image"
+    ).src = `./assets/images/dice/dice${rollNumber}.png`;
+    scoreboard();
+  });
 
-    if (randomChance < 0.495) {
-      //You fipped tails
-      document.querySelector("#penny-image").src =
-        "./assets/images/penny-tails.jpg";
-      document.querySelector("#message").textContent = "You Flipped Tails";
-      tails++;
-      total++;
-      tailPercent = Math.round((tails / total) * 100);
-      document.querySelector("#tails").textContent = tails;
-      document.querySelector("#tails-percent").textContent = tailPercent + "%";
-      if (heads > 0) {
-        headPercent = Math.round((heads / total) * 100);
-      }
-      document.querySelector("#heads").textContent = heads;
-      document.querySelector("#heads-percent").textContent = headPercent + "%";
-    } else {
-      //You flipped heads
-      document.querySelector("#penny-image").src =
-        "./assets/images/penny-heads.jpg";
-      document.querySelector("#message").textContent = "You Flipped Heads";
-      heads++;
-      total++;
-      headPercent = Math.round((heads / total) * 100);
-      document.querySelector("#heads").textContent = heads;
-      document.querySelector("#heads-percent").textContent = headPercent + "%";
-      if (tails > 0) {
-        tailPercent = Math.round((tails / total) * 100);
-      }
-      document.querySelector("#tails").textContent = tails;
-      document.querySelector("#tails-percent").textContent = tailPercent + "%";
-    }
-
-    //Reset game
-    document.querySelector("#clear").addEventListener("click", function () {
-      heads = 0;
-      tails = 0;
-      headPercent = 0;
-      tailPercent = 0;
-      total = heads + tails;
-      document.querySelector("#tails").textContent = 0;
-      document.querySelector("#tails-percent").textContent = 0 + "%";
-      document.querySelector("#heads").textContent = 0;
-      document.querySelector("#heads-percent").textContent = 0 + "%";
-      document.querySelector("#penny-image").src =
-        "./assets/images/penny-heads.jpg";
-      document.querySelector("#message").textContent = "Let's Get Flipping!";
-    });
+  //Reset game
+  document.querySelector("#clear").addEventListener("click", function () {
+    rolls = [0, 0, 0, 0, 0, 0];
+    document.querySelector("#dice-image").src =
+      "./assets/images/dice/dice1.png";
+    document.querySelector("#message").textContent = "Let's Get Rolling!";
+    scoreboard();
   });
 });
